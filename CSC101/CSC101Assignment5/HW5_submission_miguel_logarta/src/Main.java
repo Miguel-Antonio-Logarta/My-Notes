ad5c7fe9ed5ec39e4675a9d960b0f4a6fe98d55d
Miguel Logarta <maclogarta@gmail.com>
2024-02-26 13:12:41 -0800

    new work

---
+++ CSC101/CSC101Assignment5/HW5_submission_miguel_logarta/src/Main.java
@@ -0,0 +1,9 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, students of CSC 101!");
+        System.out.println("How are you all doing?");
+        System.out.println("It's such a beautiful day out there!");
+    }
+}
---
+++ CSC101/Homework6/src/Main.java
@@ -0,0 +1,19 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        String[] lines = {
+            "Moe: I find it hilarious that on one side of the world they sip on water with leaves, while on this side of the globe, we chug bean water.",
+            "Rebecca: Bean water? Did you mean coffee?",
+            "Moe: I'm calling what it truly is. It's funny how we can extract so much essence from just pressing water through grounded coffee beans.",
+            "Rebecca: Not to mention it has complex flavours and the fact that it'll always be there for you even during the worst mornings.",
+            "Moe: But don't underestimate leaf water either. The Chinese really love tea.",
+            "Rebecca: We should really make a club to exploring both. I'll call it the 'Brotherhood of Bean Water and Tea Leaf.'",
+            "Moe: Great idea!"
+        };
+
+        for (String line : lines) {
+            System.out.println(line);
+        }
+    }
+}
